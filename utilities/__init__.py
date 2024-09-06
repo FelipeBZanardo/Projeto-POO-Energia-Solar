0c991b6ac58efee080090e731190fa0db582759f
Felipe Zanardo <felipebueno6@hotmail.com>
2024-09-06 19:15:13 -0300

    feat: Criação da Classe Dataplot com seus devidos imports

--- utilities/__init__.py
+++ utilities/__init__.py
@@ -2,3 +2,4 @@ from utilities.Currency import Currency
 from utilities.DataFrameTransform import DataFrameTransform
 from utilities.DataBaseSQLite3 import DataBaseSQLite3
 from utilities.Type import Type
+from utilities.DataPlot import DataPlot
